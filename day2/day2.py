8ccc6ed1d6c0fa184c56ea79d8f71101e1f8ab84
Alex Rasterhoff <alexrasterhoff02@gmail.com>
2022-12-07 10:16:40 +0100

    day2.2 YEAHH

--- day2/day2.py
+++ day2/day2.py
@@ -27,7 +27,36 @@ with open(file_path) as file:
     for array in line_array:
         counter = counter + int(cases[array]) + int(cases[array[-1]])
         outcome = cases2[array[-1]]
-print(counter)
-print(outcome)
+# print(counter)
+# print(outcome)
 # nou dat was deel 1 maar ik moet dat hele kut ding omgooien voor deel 2 ofzo dus die werk ik er gewoon wel in ipv eronder hahaHAAA
 # doe deel 2 later wel
+# tijd om deel 2 te forceren met wacky code
+part2 = 0
+with open(file_path) as file:
+    line_array = file.readlines()
+    line_array = [item.rstrip() for item in line_array]
+    # print(line_array)
+    for array in line_array:
+        splitarray = array.split(' ')
+        p1 = splitarray[0]
+        p2 = splitarray[1]
+        if p1 == 'A' and p2 == 'X':
+            part2 += 3
+        if p1 == 'A' and p2 == 'Y':
+            part2 += 4
+        if p1 == 'A' and p2 == 'Z':
+            part2 += 8
+        if p1 == 'B' and p2 == 'X':
+            part2 += 1
+        if p1 == 'B' and p2 == 'Y':
+            part2 += 5
+        if p1 == 'B' and p2 == 'Z':
+            part2 += 9
+        if p1 == 'C' and p2 == 'X':
+            part2 += 2
+        if p1 == 'C' and p2 == 'Y':
+            part2 += 6
+        if p1 == 'C' and p2 == 'Z':
+            part2 += 7
+print(part2)
